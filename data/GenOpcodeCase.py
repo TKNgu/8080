b7b0782dbd008762d44676f9dfbc08324af16e76
ngocpt <ngocpt@bkav.com>
2023-08-11 21:16:05 +0700

    Add full opcode

--- data/GenOpcodeCase.py
+++ data/GenOpcodeCase.py
@@ -1,36 +1,68 @@
 import json
 
-print("Hello Python GenOpcode")
+# print("Hello Python GenOpcode")
+#
+# for item in [["0x06", "B"], ["0x0e", "C"], ["0x16", "D"],
+#              ["0x1e", "E"], ["0x26", "H"], ["0x2e", "L"]]:
+#     print("instructions[{}] = LD_NN_N({});".format(item[0], item[1]))
+#
+# print()
+# for item in [[0x78, "A"], [0x40, "B"], [0x48, "C"], [0x50, "D"], [0x58, "E"],
+#              [0x60, "H"], [0x68, "L"]]:
+#     code = item[0];
+#     for register in ["B", "C", "D", "E", "H", "L"]:
+#         print("instructions[{}] = LD_R1_R2({}, {});".format(hex(code), item[1], register))
+#         code += 1
+#     print("instructions[{}] = LD_R1_R2M({}, HL);".format(hex(code), item[1]))
+#     code += 1
+#     print("instructions[{}] = LD_R1_R2({}, A);".format(hex(code), item[1]))
+#     print()
+#
+# code = 0x70
+# for register in ["B", "C", "D", "E", "H", "L"]:
+#     print("instructions[{}] = LD_R1M_R2(HL, {});".format(hex(code), register))
+#     code += 1
+# print("instructions[0x36] = LD_R1M_N(HL);")
+#
+# print()
+# print("instructions[0x0a] = LD_R1_R2M(A, BC);")
+# print("instructions[0x1a] = LD_R1_R2M(A, DE);")
+# print("instructions[0x7e] = LD_R1_R2M(A, HL);")
+# print("instructions[0xfa] = LD_R1_NM(A);")
+#
+# print("instructions[0x02] = LD_R1M_R2(BC, A);")
+# print("instructions[0x12] = LD_R1M_R2(DE, A);")
+# print("instructions[0x77] = LD_R1M_R2(HL, A);")
+# print("instructions[0xea] = LD_NM_R2(A);")
 
-for item in [["0x06", "B"], ["0x0e", "C"], ["0x16", "D"],
-             ["0x1e", "E"], ["0x26", "H"], ["0x2e", "L"]]:
-    print("instructions[{}] = LD_NN_N({});".format(item[0], item[1]))
 
-print()
-for item in [[0x78, "A"], [0x40, "B"], [0x48, "C"], [0x50, "D"], [0x58, "E"],
-             [0x60, "H"], [0x68, "L"]]:
-    code = item[0];
-    for register in ["B", "C", "D", "E", "H", "L"]:
-        print("instructions[{}] = LD_R1_R2({}, {});".format(hex(code), item[1], register))
-        code += 1
-    print("instructions[{}] = LD_R1_R2M({}, HL);".format(hex(code), item[1]))
-    code += 1
-    print("instructions[{}] = LD_R1_R2({}, A);".format(hex(code), item[1]))
+print("*************************************")
+REGISTER = ["B", "C", "D", "E", "H", "L", "HL", "A"] 
+code = 0x40
+for r1 in REGISTER:
     print()
+    for r2 in REGISTER:
+        if r1 == r2:
+            print("instructions[{}] = LD_RR;".format(hex(code)));
+        elif r1 == "HL":
+            print("instructions[{}] = LD_VR1_R2({}, {});".format(hex(code), r1, r2));
+        elif r2 == "HL":
+            print("instructions[{}] = LD_R1_VR2({}, {});".format(hex(code), r1, r2));
+        else:
+            print("instructions[{}] = LD_R1_R2({}, {});".format(hex(code), r1, r2));
+        code += 1
 
-code = 0x70
-for register in ["B", "C", "D", "E", "H", "L"]:
-    print("instructions[{}] = LD_R1M_R2(HL, {});".format(hex(code), register))
-    code += 1
-print("instructions[0x36] = LD_R1M_N(HL);")
+OP = ["ADD", "ADC", "SUB", "SBC", "AND", "XOR", "OR", "CP"]
+code = 0x80
+for op in OP:
+    print()
+    for r in REGISTER:
+        if r == "HL":
+            print("instructions[{}] = {}_R1_R2(A, memory[{}]);".format(hex(code), op, r))
+        else:
+            print("instructions[{}] = {}_R1_R2(A, {});".format(hex(code), op, r))
+        code += 1
 
-print()
-print("instructions[0x0a] = LD_R1_R2M(A, BC);")
-print("instructions[0x1a] = LD_R1_R2M(A, DE);")
-print("instructions[0x7e] = LD_R1_R2M(A, HL);")
-print("instructions[0xfa] = LD_R1_NM(A);")
 
-print("instructions[0x02] = LD_R1M_R2(BC, A);")
-print("instructions[0x12] = LD_R1M_R2(DE, A);")
-print("instructions[0x77] = LD_R1M_R2(HL, A);")
-print("instructions[0xea] = LD_NM_R2(A);")
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -53,9 +53,9 @@ struct CPURegister {
       uint8_t S;
       uint8_t P;
     };
-    uint16_t SP = 0xffff;
+    uint16_t SP;
   };
-  uint16_t PC = 0x0100;
+  uint16_t PC;
 } CPURegister;
 
 #define ZFLAG CPURegister.F |= 0x10000000
@@ -125,9 +125,75 @@ unsigned char memory[0xffff];
   []() {                                                 \
     uint32_t result = R1 + R2;                           \
     (result & 0xff00) ? CFLAG : CCLEAR;                  \
-    NFLAG;                                               \
+    NCLEAR;                                              \
     ((R1 & 0x0f) + (R2 & 0x0f)) > 0x0f ? HFLAG : HCLEAR; \
+    R1 = result;                                         \
     return 8u;                                           \
+  };
+
+#define ADC_R1_R2(R1, R2)                                     \
+  []() {                                                      \
+    uint8_t cy = CFLAG ? 1u : 0u;                             \
+    uint32_t result = R1 + R2 + cy;                           \
+    (result & 0xff00) ? CFLAG : CCLEAR;                       \
+    NCLEAR;                                                   \
+    ((R1 & 0x0f) + (R2 & 0x0f)) + cy > 0x0f ? HFLAG : HCLEAR; \
+    R1 = result;                                              \
+    return 8u;                                                \
+  };
+
+#define SUB_R1_R2(R1, R2)                                \
+  []() {                                                 \
+    int32_t result = R1 - R2;                            \
+    (result & 0xff00) ? CFLAG : CCLEAR;                  \
+    NFLAG;                                               \
+    ((R1 & 0x0f) - (R2 & 0x0f)) & 0xf0 ? HFLAG : HCLEAR; \
+    R1 = result;                                         \
+    return 8u;                                           \
+  };
+
+#define SBC_R1_R2(R1, R2)                                     \
+  []() {                                                      \
+    uint8_t cy = CFLAG ? 1u : 0u;                             \
+    uint32_t result = R1 - R2 - cy;                           \
+    (result & 0xff00) ? CFLAG : CCLEAR;                       \
+    NFLAG;                                                    \
+    ((R1 & 0x0f) - (R2 & 0x0f)) - cy & 0x0f ? HFLAG : HCLEAR; \
+    R1 = result;                                              \
+    return 8u;                                                \
+  };
+
+#define AND_R1_R2(R1, R2) \
+  []() {                  \
+    F = 0b00100000;       \
+    R1 &= R2;             \
+    R1 ? ZCLEAR : ZFLAG;  \
+    return 4u;            \
+  };
+
+#define XOR_R1_R2(R1, R2) \
+  []() {                  \
+    F = 0b00000000;       \
+    R1 ^= R2;             \
+    R1 ? ZCLEAR : ZFLAG;  \
+    return 4u;            \
+  };
+
+#define OR_R1_R2(R1, R2) \
+  []() {                 \
+    F = 0b00000000;      \
+    R1 |= R2;            \
+    R1 ? ZCLEAR : ZFLAG; \
+    return 4u;           \
+  };
+
+#define CP_R1_R2(R1, R2)                        \
+  []() {                                        \
+    R1 == R2 ? ZFLAG : ZCLEAR;                  \
+    R1 > R2 ? CCLEAR : CFLAG;                   \
+    (R1 & 0x0f) > (R2 & 0x0f) ? HCLEAR : HFLAG; \
+    NFLAG;                                      \
+    return 4u;                                  \
   };
 
 #define DEC_R(R) \
@@ -136,9 +202,22 @@ unsigned char memory[0xffff];
     return 8u;   \
   };
 
-#define JR_OFFSET                  \
-  PC += (signed char)(memory[PC]); \
+#define JR_OFFSET           \
+  auto tmp = memory[PC];    \
+  PC += (signed char)(tmp); \
   return 12u;
+
+#define LD_R1_R2(R1, R2) \
+  []() {                 \
+    R1 = R2;             \
+    return 4u;           \
+  };
+
+#define LD_RR []() { return 4u; };
+
+#define HALT []() { return 4u; };
+
+#define COUT_HEX(value) cout << hex << setw(2) << setfill('0') << (int)(value)
 
 function<unsigned()> instructions[256];
 
@@ -236,14 +315,270 @@ void LoadInstructions() {
     }
     JR_OFFSET;
   };
-
-  // Dang them
-  instructions[0x32] = []() {
-    memory[HL] = A;
-    HL--;
+  instructions[0x21] = LD_R_N16(H, L);
+  instructions[0x22] = []() {
+    memory[HL++] = A;
     return 8u;
   };
-  instructions[0x21] = LD_R_N16(H, L);
+  instructions[0x23] = INC_R(HL);
+  instructions[0x24] = INC_R_FLAG(H);
+  instructions[0x25] = DEC_R_FLAG(H);
+  instructions[0x26] = LD_R_N8(H);
+  instructions[0x27] = []() {
+    auto tmp = A;
+    if (NFLAG) {
+      if (HFLAG) {
+        tmp = (tmp - 0x06) & 0xff;
+      }
+      if (CFLAG) {
+        tmp -= 0x60;
+      }
+    } else {
+      if (HFLAG || (tmp & 0xf) > 9) {
+        tmp += 0x06;
+      }
+      if (CFLAG || tmp > 0x97) {
+        tmp += 0x60;
+      }
+    }
+    A = tmp;
+    HCLEAR;
+    A ? ZCLEAR : ZFLAG;
+    tmp >= 0x0100 ? CFLAG : CCLEAR;
+    return 4u;
+  };
+  instructions[0x28] = []() {
+    if (ZFLAG) {
+      JR_OFFSET;
+    }
+    PC++;
+    return 8u;
+  };
+  instructions[0x29] = ADD_R1_R2(HL, HL);
+  instructions[0x2a] = []() {
+    A = memory[HL++];
+    return 8u;
+  };
+  instructions[0x2b] = DEC_R(HL);
+  instructions[0x2c] = INC_R_FLAG(L);
+  instructions[0x2d] = DEC_R_FLAG(L);
+  instructions[0x2e] = LD_R_N8(L);
+  instructions[0x2f] = []() {
+    A ^= 0xff;
+    F |= 0x01100000;
+    return 4u;
+  };
+  instructions[0x30] = []() {
+    if (CFLAG) {
+      PC++;
+      return 8u;
+    }
+    JR_OFFSET;
+  };
+  instructions[0x31] = LD_R_N16(S, P);
+  instructions[0x32] = []() {
+    memory[HL--] = A;
+    return 8u;
+  };
+  instructions[0x33] = INC_R(SP);
+  instructions[0x34] = []() {
+    uint8_t value = memory[HL] + 1;
+    value ? ZCLEAR : ZFLAG;
+    NCLEAR;
+    (value & 0x0f) ? HCLEAR : HFLAG;
+    memory[HL] = value;
+    return 12u;
+  };
+  instructions[0x35] = []() {
+    uint8_t value = memory[HL] - 1;
+    value ? ZCLEAR : ZFLAG;
+    NCLEAR;
+    ((value & 0x0f) != 0xf) ? HCLEAR : HFLAG;
+    memory[HL] = value;
+    return 12u;
+  };
+  instructions[0x36] = []() {
+    memory[HL] = memory[PC++];
+    return 12u;
+  };
+  instructions[0x37] = []() {
+    F = (F & 0x10000000) | 0x00010000;
+    return 4u;
+  };
+  instructions[0x38] = []() {
+    if (CFLAG) {
+      JR_OFFSET;
+    }
+    PC++;
+    return 8u;
+  };
+  instructions[0x39] = []() {
+    uint32_t tmp = HL + SP;
+    NCLEAR;
+    tmp > 0xffff ? CFLAG : CCLEAR;
+    (L + P) > 0xff ? HFLAG : HCLEAR;
+    HL = tmp;
+    return 8u;
+  };
+  instructions[0x3a] = []() {
+    A = memory[HL--];
+    return 8u;
+  };
+  instructions[0x3b] = DEC_R(SP);
+  instructions[0x3c] = INC_R_FLAG(A);
+  instructions[0x3d] = DEC_R_FLAG(A);
+  instructions[0x3e] = LD_R_N8(A);
+  instructions[0x3f] = []() {
+    (F & 0x00010000) ? CCLEAR : CFLAG;
+    F &= 0x10010000;
+    return 4u;
+  };
+
+  instructions[0x40] = LD_RR;
+  instructions[0x41] = LD_R1_R2(B, C);
+  instructions[0x42] = LD_R1_R2(B, D);
+  instructions[0x43] = LD_R1_R2(B, E);
+  instructions[0x44] = LD_R1_R2(B, H);
+  instructions[0x45] = LD_R1_R2(B, L);
+  instructions[0x46] = LD_R1_VR2(B, HL);
+  instructions[0x47] = LD_R1_R2(B, A);
+
+  instructions[0x48] = LD_R1_R2(C, B);
+  instructions[0x49] = LD_RR;
+  instructions[0x4a] = LD_R1_R2(C, D);
+  instructions[0x4b] = LD_R1_R2(C, E);
+  instructions[0x4c] = LD_R1_R2(C, H);
+  instructions[0x4d] = LD_R1_R2(C, L);
+  instructions[0x4e] = LD_R1_VR2(C, HL);
+  instructions[0x4f] = LD_R1_R2(C, A);
+
+  instructions[0x50] = LD_R1_R2(D, B);
+  instructions[0x51] = LD_R1_R2(D, C);
+  instructions[0x52] = LD_RR;
+  instructions[0x53] = LD_R1_R2(D, E);
+  instructions[0x54] = LD_R1_R2(D, H);
+  instructions[0x55] = LD_R1_R2(D, L);
+  instructions[0x56] = LD_R1_VR2(D, HL);
+  instructions[0x57] = LD_R1_R2(D, A);
+
+  instructions[0x58] = LD_R1_R2(E, B);
+  instructions[0x59] = LD_R1_R2(E, C);
+  instructions[0x5a] = LD_R1_R2(E, D);
+  instructions[0x5b] = LD_RR;
+  instructions[0x5c] = LD_R1_R2(E, H);
+  instructions[0x5d] = LD_R1_R2(E, L);
+  instructions[0x5e] = LD_R1_VR2(E, HL);
+  instructions[0x5f] = LD_R1_R2(E, A);
+
+  instructions[0x60] = LD_R1_R2(H, B);
+  instructions[0x61] = LD_R1_R2(H, C);
+  instructions[0x62] = LD_R1_R2(H, D);
+  instructions[0x63] = LD_R1_R2(H, E);
+  instructions[0x64] = LD_RR;
+  instructions[0x65] = LD_R1_R2(H, L);
+  instructions[0x66] = LD_R1_VR2(H, HL);
+  instructions[0x67] = LD_R1_R2(H, A);
+
+  instructions[0x68] = LD_R1_R2(L, B);
+  instructions[0x69] = LD_R1_R2(L, C);
+  instructions[0x6a] = LD_R1_R2(L, D);
+  instructions[0x6b] = LD_R1_R2(L, E);
+  instructions[0x6c] = LD_R1_R2(L, H);
+  instructions[0x6d] = LD_RR;
+  instructions[0x6e] = LD_R1_VR2(L, HL);
+  instructions[0x6f] = LD_R1_R2(L, A);
+
+  instructions[0x70] = LD_VR1_R2(HL, B);
+  instructions[0x71] = LD_VR1_R2(HL, C);
+  instructions[0x72] = LD_VR1_R2(HL, D);
+  instructions[0x73] = LD_VR1_R2(HL, E);
+  instructions[0x74] = LD_VR1_R2(HL, H);
+  instructions[0x75] = LD_VR1_R2(HL, L);
+  instructions[0x76] = LD_RR;
+  instructions[0x77] = LD_VR1_R2(HL, A);
+
+  instructions[0x78] = LD_R1_R2(A, B);
+  instructions[0x79] = LD_R1_R2(A, C);
+  instructions[0x7a] = LD_R1_R2(A, D);
+  instructions[0x7b] = LD_R1_R2(A, E);
+  instructions[0x7c] = LD_R1_R2(A, H);
+  instructions[0x7d] = LD_R1_R2(A, L);
+  instructions[0x7e] = LD_R1_VR2(A, HL);
+  instructions[0x7f] = LD_RR;
+
+  instructions[0x80] = ADD_R1_R2(A, B);
+  instructions[0x81] = ADD_R1_R2(A, C);
+  instructions[0x82] = ADD_R1_R2(A, D);
+  instructions[0x83] = ADD_R1_R2(A, E);
+  instructions[0x84] = ADD_R1_R2(A, H);
+  instructions[0x85] = ADD_R1_R2(A, L);
+  instructions[0x86] = ADD_R1_R2(A, memory[HL]);
+  instructions[0x87] = ADD_R1_R2(A, A);
+
+  instructions[0x88] = ADC_R1_R2(A, B);
+  instructions[0x89] = ADC_R1_R2(A, C);
+  instructions[0x8a] = ADC_R1_R2(A, D);
+  instructions[0x8b] = ADC_R1_R2(A, E);
+  instructions[0x8c] = ADC_R1_R2(A, H);
+  instructions[0x8d] = ADC_R1_R2(A, L);
+  instructions[0x8e] = ADC_R1_R2(A, memory[HL]);
+  instructions[0x8f] = ADC_R1_R2(A, A);
+
+  instructions[0x90] = SUB_R1_R2(A, B);
+  instructions[0x91] = SUB_R1_R2(A, C);
+  instructions[0x92] = SUB_R1_R2(A, D);
+  instructions[0x93] = SUB_R1_R2(A, E);
+  instructions[0x94] = SUB_R1_R2(A, H);
+  instructions[0x95] = SUB_R1_R2(A, L);
+  instructions[0x96] = SUB_R1_R2(A, memory[HL]);
+  instructions[0x97] = SUB_R1_R2(A, A);
+
+  instructions[0x98] = SBC_R1_R2(A, B);
+  instructions[0x99] = SBC_R1_R2(A, C);
+  instructions[0x9a] = SBC_R1_R2(A, D);
+  instructions[0x9b] = SBC_R1_R2(A, E);
+  instructions[0x9c] = SBC_R1_R2(A, H);
+  instructions[0x9d] = SBC_R1_R2(A, L);
+  instructions[0x9e] = SBC_R1_R2(A, memory[HL]);
+  instructions[0x9f] = SBC_R1_R2(A, A);
+
+  instructions[0xa0] = AND_R1_R2(A, B);
+  instructions[0xa1] = AND_R1_R2(A, C);
+  instructions[0xa2] = AND_R1_R2(A, D);
+  instructions[0xa3] = AND_R1_R2(A, E);
+  instructions[0xa4] = AND_R1_R2(A, H);
+  instructions[0xa5] = AND_R1_R2(A, L);
+  instructions[0xa6] = AND_R1_R2(A, memory[HL]);
+  instructions[0xa7] = AND_R1_R2(A, A);
+
+  instructions[0xa8] = XOR_R1_R2(A, B);
+  instructions[0xa9] = XOR_R1_R2(A, C);
+  instructions[0xaa] = XOR_R1_R2(A, D);
+  instructions[0xab] = XOR_R1_R2(A, E);
+  instructions[0xac] = XOR_R1_R2(A, H);
+  instructions[0xad] = XOR_R1_R2(A, L);
+  instructions[0xae] = XOR_R1_R2(A, memory[HL]);
+  instructions[0xaf] = XOR_R1_R2(A, A);
+
+  instructions[0xb0] = OR_R1_R2(A, B);
+  instructions[0xb1] = OR_R1_R2(A, C);
+  instructions[0xb2] = OR_R1_R2(A, D);
+  instructions[0xb3] = OR_R1_R2(A, E);
+  instructions[0xb4] = OR_R1_R2(A, H);
+  instructions[0xb5] = OR_R1_R2(A, L);
+  instructions[0xb6] = OR_R1_R2(A, memory[HL]);
+  instructions[0xb7] = OR_R1_R2(A, A);
+
+  instructions[0xb8] = CP_R1_R2(A, B);
+  instructions[0xb9] = CP_R1_R2(A, C);
+  instructions[0xba] = CP_R1_R2(A, D);
+  instructions[0xbb] = CP_R1_R2(A, E);
+  instructions[0xbc] = CP_R1_R2(A, H);
+  instructions[0xbd] = CP_R1_R2(A, L);
+  instructions[0xbe] = CP_R1_R2(A, memory[HL]);
+  instructions[0xbf] = CP_R1_R2(A, A);
+
+  // Dang them
   instructions[0xc3] = []() {
     PC = memory[PC] + (memory[PC + 1] << 8);
     return 16u;
@@ -253,6 +588,19 @@ void LoadInstructions() {
     ZFLAG;
     return 4u;
   };
+}
+
+void BootRom() {
+  CPURegister.A = 0x01;
+  CPURegister.F = 0x00;
+  CPURegister.B = 0xff;
+  CPURegister.C = 0x13;
+  CPURegister.D = 0x00;
+  CPURegister.E = 0xc1;
+  CPURegister.H = 0x84;
+  CPURegister.L = 0x03;
+  CPURegister.PC = 0x100;
+  CPURegister.SP = 0xfffe;
 }
 
 int main(int argc, char *argv[]) {
@@ -285,19 +633,31 @@ int main(int argc, char *argv[]) {
        << endl;
 
   LoadInstructions();
-  unsigned count = 0;
+  BootRom();
   for (;;) {
-    cout << "0x" << hex << setw(4) << (int)(CPURegister.PC) << " 0x" << hex
-         << setw(2) << (int)(memory[CPURegister.PC]) << endl;
-    instructions[memory[CPURegister.PC++]]();
-    if (!memory[CPURegister.PC]) {
-      if (count++ > 30) {
-        count = 0;
-        getchar();
-      }
-    } else {
-      count = 0;
+    // // if (CPURegister.PC == 0x0216) {
+    // cout << "Debug CPU **********" << endl;
+    cout << "PC " << (int)(CPURegister.PC) << endl;
+    cout << "Opcode 0x" << hex << setw(2) << setfill('0')
+         << (int)(memory[CPURegister.PC]) << endl;
+    // // cout << "ZFlag " << (int)(ZFLAG) << endl;
+    // // cout << "Memory " << (int)(memory[CPURegister.PC + 1]) << endl;
+    // // }
+    //
+    if (CPURegister.PC == 0x0217) {
+      getchar();
     }
+    //
+    // if (!memory[0x0217]) {
+    //   cout << "Memory reset to 0" << endl;
+    // }
+    //
+    // if (CPURegister.HL == 0x0217) {
+    //   cout << "HL chang to data ********** " << endl;
+    //   getchar();
+    // }
+
+    instructions[memory[CPURegister.PC++]]();
   }
 
   return 0;
